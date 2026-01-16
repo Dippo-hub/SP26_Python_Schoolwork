bacf398f760f971d71e3194d1ddbda2ce1d1cba8
Nathan Silvey <nsilvey2006@gmail.com>
2026-01-16 19:43:40 +0000

    Add 1_16_26.py

---
+++ 1_16_26.py
@@ -0,0 +1,9 @@
+#Attempt 1 Python
+my_name="Nathan Silvey"
+print("My name is: "+my_name+"\n")
+print("Use 4 spaces per indentation level.\n")
+print("Identifiers are unlimited in length. Case is significant.\n")
+print("Three goals I have for this course are 1) to be comfortable coding in python,\
+       2) to be able to create basic applications for my own convenience, and \
+      3) to pass the class and get the credit hours.")
+#Push to git
