a32acb4bc7f42096e0536de10bf1ccdaf89e73a9
Nathan Silvey <nsilvey2006@gmail.com>
2026-02-18 19:53:41 +0000

    2/18

--- CardSorter.py
+++ CardSorter.py
@@ -6,7 +6,8 @@ deck={}
 decklist=[]
 
 import re
-
+import fileinput
+##Need to update to work with file
 def editDeck():
   decklist=input("Enter decklist file: ")
   try:
@@ -25,29 +26,54 @@ def editDeck():
     print(f"Unsure: {unsure}\n")
     print(f"Added: {added}\n")
     print(f"Removed: {removed}\n")
+     
     
     
   except Exception as e:
      print(f"An error occurred: {e}")
-  for card in added:
-    for item in deck:
-      if item==card:
-        deck[item]+=1
-      else:
-        deck.update(card, 1)
-  print(deck)
+  try:
+    with open('decks.txt','a') as file:
+      stop_count=0
+      commander_=input("Which commander to edit? ")
+      for card___ in file.read().split('\n'):
+       for to_be in added:
+        match_c=re.search(commander_, card___)
+        match_stop=re.search("Commander:", card___)
+        if match_c:
+          if found==True:
+            continue
+          else:
+           found=True
+           print(f'Found {card___}')
+        if found==True:
+           if not match_stop:
+            target_keyword = commander_  # The header you're looking for
+            text_to_add = to_be 
+
+# inplace=True redirects stdout to the file itself
+            for line in fileinput.input('decks.txt', inplace=True):
+             print(line, end="")  # Print original line back to file
+            if target_keyword in line:
+              print(text_to_add)
+              continue
+        elif stop_count==0:
+         stop_count+=1
+        else:
+          break
+            
+           
+    if found==False:
+     print(f'Card {cut} not found.')
+    
+  except Exception as e:
+      print(F"An error occured: {e}")
 
 def getDeck():
   deck={} 
   action=input("Enter cards by (T)ext or by (F)ile? ").upper()
   if action=='T':
     cards=input("Enter deck as plain text: ").split('\n')
-    for card in cards:
-      for item in deck:
-        if item==card:
-          deck[item]+=1
-        else:
-          deck.update(card, 1)
+    ##Handling for text entry needed.
     print(deck)
   if action=='F':
     stuff=input("Enter decklist file name: ")
@@ -78,15 +104,32 @@ def getDeck():
 
 def viewDeck():
   commander=input("Enter commander: ")
+  found=False
   try:
     with open('decks.txt','r') as file:
+      stop_count=0
       for card__ in file.read().split('\n'):
-        if card__==commander:
-          match=re.search(r"(\d+)\s*(.*)", card__)
-          if match:
-           quantity = int(match.group(1))
-           name = match.group(2)
-           deck.update({name : quantity})
+        match_c=re.search(commander, card__)
+        match_stop=re.search("Commander:", card__)
+        if match_c:
+          if found==True:
+            continue
+          else:
+           found=True
+           print(f'Found {card__}')
+        if found==True:
+           if not match_stop:
+            print(card__)
+            continue
+           elif stop_count==0:
+             stop_count+=1
+           else:
+             break
+            
+           
+    if found==False:
+     print(f'Commander {commander} not found.')
+    
   except Exception as e:
       print(F"An error occured: {e}")
 
