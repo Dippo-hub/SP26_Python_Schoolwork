b00c31ba4623a5c2d16dfdec3016be89b4c49763
Nathan Silvey <nsilvey2006@gmail.com>
2026-08-24 21:13:16 +0000

    8-24.2

--- FA26/OOP/8-24.cpp
+++ FA26/OOP/8-24.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define PI = 3.1415
+
 int math_out () {
     std::cout << "(7.2 + 3 * 5) / (23-7.8) = ";
     std::cout << (7.2 + 3 * 5) / (23-7.8) << std::endl;
@@ -11,10 +13,12 @@ int math_out () {
 void size_of() {
     std::cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
     std::cout << "Size of short: " << sizeof(short) << " bytes" << std::endl;
+    std::cout << "Size of bool: " << sizeof(bool) << " bytes" << std::endl;
 }
 
 int main () {
     math_out();
     size_of();
+    std::cout << (true == 0) <<std::endl; //true=1, false=0
     return 0;
 }
